4f5fcfa42afbf6389d54bfd13ec982a88453c3cc
Raphael Marco <raphaelmarco@outlook.com>
2019-08-11 00:52:39 +0800

    Fix bot repeating messages on non-dm channels

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import discord
 
+
 class DiscordWrapper(discord.Client):
     def __init__(self, loop, guild_id):
         self.guild_id = int(guild_id)
@@ -11,10 +12,3 @@ class DiscordWrapper(discord.Client):
 
         self.guild = self.get_guild(self.guild_id)
         print('Connected to guild "%s"' % self.guild.name)
-
-    async def on_message(self, message):
-        # don't respond to ourselves
-        if message.author == self.user:
-            return
-
-        await message.channel.send(message.content)
